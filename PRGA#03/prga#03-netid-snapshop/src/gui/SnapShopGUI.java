3125d5b45c690e10b8a46c1766fd70440965f5b0
AnyLeftovers <37920527+yudonglin@users.noreply.github.com>
2021-12-01 20:02:41 -0800

    add comments and some new improvements

--- PRGA#03/prga#03-netid-snapshop/src/gui/SnapShopGUI.java
+++ PRGA#03/prga#03-netid-snapshop/src/gui/SnapShopGUI.java
@@ -1,5 +1,3 @@
-// finish (and comment) me!
-
 package gui;
 
 import filters.*;
@@ -11,89 +9,89 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.util.HashMap;
 
+
 /**
- * @author ???
- * @version ???
+ * a class that extends JFrame and manage the gui and Image modifier
  */
 public class SnapShopGUI extends JFrame {
 
+    // the main panel of the frame
     private final JPanel screen = new JPanel();
-
+    // the hash map that will be used to store buttons for filter
     private final HashMap<String, JButton> imageActionButtonsHashMap = new HashMap<>(7);
-
+    // the JFileChooser that will be used to select files
     private final JFileChooser imageChooser = new JFileChooser(FileSystems.getDefault().getPath(".").toString());
+    // open button
     private final JButton openButton = new JButton("Open...");
+    // save button
     private final JButton saveAsButton = new JButton("Save As...");
+    // close button
     private final JButton closeImageButton = new JButton("Close Image");
+    // a JLabel that will be used to show image
     private final JLabel imageViewer = new JLabel();
+    // the image that is current modifying
     private PixelImage imageIsModifying;
+    // the extension of the image that is current modifying
+    private String extensionOfCurrentImage;
 
-    public SnapShopGUI() {
-    }
 
     /**
-     *
+     * initialize the gui and critical data
      */
     public void start() {
         //javax.swing.JOptionPane.showMessageDialog(null, "SnapShop placeholder");
-        init();
-    }
-
-    private void setActionButtonsEnabled(boolean status) {
-        for (JButton actionButton : imageActionButtonsHashMap.values()) {
-            actionButton.setEnabled(status);
-        }
-        saveAsButton.setEnabled(status);
-        closeImageButton.setEnabled(status);
-    }
-
-
-    public void init() {
 
         this.setTitle("TCSS 305 – Programming Assignment 3 (ydlin)");
         this.setIconImage(Toolkit.getDefaultToolkit().getImage(FileSystems.getDefault().getPath("sample_images/huskies_logo.jpg").toAbsolutePath().toString()));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // add the main panel to the frame
         this.add(screen);
-        screen.removeAll();
+
+        // set main panel layout to BorderLayout
         screen.setLayout(new BorderLayout());
 
         // init upper panel
-        var upPanel = new JPanel();
-        screen.add(upPanel, BorderLayout.NORTH);
-        //add buttons
-        var imageActionButtonsStr = new AbstractFilter[]{
+        var upperPanel = new JPanel();
+        screen.add(upperPanel, BorderLayout.NORTH);
+        // init and create filters
+        AbstractFilter[] imageActionButtonsStr = {
                 new EdgeDetectFilter(), new EdgeHighlightFilter(), new FlipHorizontalFilter(), new FlipVerticalFilter(), new GrayscaleFilter(), new SharpenFilter(), new SoftenFilter()
         };
-        imageActionButtonsHashMap.clear();
+        // init buttons (I use a hash map to store all the buttons)
         for (AbstractFilter filter_t : imageActionButtonsStr) {
+            // create a button
             var button_t = new JButton(filter_t.getDescription());
             button_t.setEnabled(false);
+            // add ActionListener to target button, so it will know which filter to use
             button_t.addActionListener(e -> {
                 filter_t.filter(imageIsModifying);
+                // reset the imageViewer show it will show updated image
                 imageViewer.setIcon(new ImageIcon(imageIsModifying));
             });
+            // put the button into the map
             imageActionButtonsHashMap.put(filter_t.getDescription(), button_t);
-            upPanel.add(button_t);
+            // add the button to the upper panel
+            upperPanel.add(button_t);
         }
 
+        // middle panel will be used to store imageViewer (which is used to display image)
         var middleImagePanel = new JPanel();
         screen.add(middleImagePanel, BorderLayout.CENTER);
         middleImagePanel.add(imageViewer);
 
 
-        imageActionButtonsHashMap.get("Edge Detect").addActionListener(e -> {
-            new EdgeDetectFilter().filter(imageIsModifying);
-            imageViewer.setIcon(new ImageIcon(imageIsModifying));
-        });
-
         // init lower panel
         var lowerPanel = new JPanel();
         screen.add(lowerPanel, BorderLayout.SOUTH);
         // add buttons
+        openButton.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().getImage(FileSystems.getDefault().getPath("icons/open.gif").toAbsolutePath().toString())));
         lowerPanel.add(openButton);
         saveAsButton.setEnabled(false);
+        saveAsButton.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().getImage(FileSystems.getDefault().getPath("icons/save.gif").toAbsolutePath().toString())));
         lowerPanel.add(saveAsButton);
         closeImageButton.setEnabled(false);
+        closeImageButton.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().getImage(FileSystems.getDefault().getPath("icons/close.gif").toAbsolutePath().toString())));
         lowerPanel.add(closeImageButton);
 
         // add ActionListener to open button
@@ -103,36 +101,72 @@ public class SnapShopGUI extends JFrame {
                 try {
                     imageIsModifying = PixelImage.load(file);
                     imageViewer.setIcon(new ImageIcon(imageIsModifying));
+                    this.updateExtension(file.getAbsolutePath());
                     this.setActionButtonsEnabled(true);
                 } catch (IOException ex) {
-                    JOptionPane.showMessageDialog(this, "The select filed " + file.getAbsolutePath() + " did not contain an image!", "Error!", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(this, "The selected file " + file.getAbsolutePath() + " did not contain an image!", "Error!", JOptionPane.ERROR_MESSAGE);
                 }
             }
             this.pack();
         });
 
+        // add ActionListener to save button
         saveAsButton.addActionListener(e -> {
             if (imageChooser.showSaveDialog(this) == JFileChooser.APPROVE_OPTION) {
                 var file = imageChooser.getSelectedFile();
-                try {
-                    imageIsModifying.save(file);
-                    this.setActionButtonsEnabled(true);
-                } catch (IOException ex) {
-                    JOptionPane.showMessageDialog(this, "Eggs are not supposed to be green.", "Error!", JOptionPane.ERROR_MESSAGE);
+                // check whether the user keeps the same extension, if not, then need to double-check with the user
+                if (file.getPath().endsWith(extensionOfCurrentImage) || JOptionPane.showConfirmDialog(this, "Saving the image with different extension is highly not recommended, are you sure that you want to continue?", "Warning!", JOptionPane.YES_NO_OPTION) == 0) {
+                    try {
+                        imageIsModifying.save(file);
+                        this.setActionButtonsEnabled(true);
+                    } catch (IOException ex) {
+                        JOptionPane.showMessageDialog(this, "Fail to save image.", "Error!", JOptionPane.ERROR_MESSAGE);
+                    }
                 }
             }
-            this.pack();
         });
 
+        // add ActionListener to close button
         closeImageButton.addActionListener(e -> {
+            // stop showing image
             imageViewer.setIcon(null);
+            // disable all button except open button
             this.setActionButtonsEnabled(false);
+            // resize window
             this.pack();
         });
 
-        this.setVisible(true);
+        // resize window
         this.pack();
+        // set minimum size of the frame
+        this.setMinimumSize(this.getSize());
+        // start showing content
+        this.setVisible(true);
+
     }
 
+    /**
+     * enable or disable all buttons except the open button
+     *
+     * @param status whether enable or disable all buttons
+     */
+    private void setActionButtonsEnabled(boolean status) {
+        for (JButton actionButton : imageActionButtonsHashMap.values()) {
+            actionButton.setEnabled(status);
+        }
+        saveAsButton.setEnabled(status);
+        closeImageButton.setEnabled(status);
+    }
 
+    /**
+     * store the newly loaded image's extension for saving checking
+     *
+     * @param _path the path of newly loaded image
+     */
+    private void updateExtension(String _path) {
+        int _index = _path.lastIndexOf('.');
+        if (_index > 0) {
+            extensionOfCurrentImage = _path.substring(_index);
+        }
+    }
 }
